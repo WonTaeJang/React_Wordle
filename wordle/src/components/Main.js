fd22c6b25d412c37b6f8fa13bc3a67b4c943c1c8
WonTaeJang <jwt0212@gmail.com>
2022-04-08 18:40:54 +0900

    step으로 단계를 나누고 step 에는 5개의 알파벳이 들어갈 수 있으며 해당 알파벳 리스트는 0~5개만 입력가능

--- wordle/src/components/Main.js
+++ wordle/src/components/Main.js
@@ -6,6 +6,7 @@ function Main(props) {
     // step에 따른 textbox 
     let [step, setStep] = useState(0);
     let [word, setWord] = useState({ word: '' });
+    let [wordList, setWordList] = useState({'step0':[]});
     let [wordNum, setWordNum] = useState(0);
     let keyArray = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P",
         "A", "S", "D", "F", "G", "H", "J", "K", "L",
@@ -18,29 +19,44 @@ function Main(props) {
 
         switch (word.word) {
             case "ENTER": break;
-            case "BACKSPACE": break;
+            case "BACKSPACE": 
+                if(wordList[`step${step}`].length == 0) return;
+
+                let deleteArray = [...wordList[`step${step}`]];
+                deleteArray.pop();
+                let tempDelObj = {};
+                tempDelObj[`step${step}`] = deleteArray;
+
+                setWordList(tempDelObj);
+                break;
             default:
-                console.log(word);
-                console.log(word);
-                console.log(word);
+                // step 당 알파벳은 5개까지
+                if(wordList[`step${step}`].length >= 5) return;
+
+                let tempArray = [...wordList[`step${step}`]];
+                tempArray.push(word.word);
+
+                let tempObj = {};
+                tempObj[`step${step}`] = tempArray;
+
+                setWordList(tempObj);
+                
+
                 break;
         }
 
     }, [word])   // word 값이 변할때만 반응
 
+    useEffect(()=>{
+        console.log('hi: ', wordList);
+    }, [wordList])
+
     useEffect(() => {
         window.addEventListener("keydown", (e) => {
             if (keyArray.includes(e.key.toUpperCase())) {
-                //console.log(e.key);
-                switch (e.key.toUpperCase()) {
-                    case "BACKSPACE": break;
-                    case "ENTER": break;
-                    default:
-                        // 이벤트 리스너를 사용할경우 usestate의 set만 가능하고 변수값을 가져올 수 없다.
-                        // 그래서 set을 한 후 useEffect로 값을 읽는 방식을 채택
-                        setWord({ word: e.key.toUpperCase() });
-                        break;
-                }
+                // 이벤트 리스너를 사용할경우 usestate의 set만 가능하고 변수값을 가져올 수 없다.
+                // 그래서 set을 한 후 useEffect로 값을 읽는 방식을 채택
+                setWord({ word: e.key.toUpperCase() });
 
             }
         })
