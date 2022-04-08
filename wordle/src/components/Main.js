065c3aa139b1268560dc2a881d2bf41e9412a4bd
장원태 <wt-jang@jang-wontaeui-MacBookAir.local>
2022-04-08 11:07:54 +0900

    test2

--- wordle/src/components/Main.js
+++ wordle/src/components/Main.js
@@ -21,6 +21,7 @@ function Main(props) {
             case "BACKSPACE": break;
             default:
                 console.log(word);
+                console.log(word);
                 break;
         }
 
